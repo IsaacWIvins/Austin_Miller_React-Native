d6606926838506c4a0d4a0ff7fe99f0b729d0622
Isaac Ivins <isaacivins@Isaacs-MacBook-Air.local>
2017-06-30 20:52:54 -0400

    connect page content, plus links to SM pages

--- austin-miller-react-native/screens/contact.js
+++ austin-miller-react-native/screens/contact.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, ScrollView} from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView, Linking, TouchableOpacity} from 'react-native';
 
 export default class Connect extends React.Component {
   render() {
@@ -8,27 +8,63 @@ export default class Connect extends React.Component {
         <View style={styles.imgContainer}>
           <Image source={require('../assets/images/headshot.jpg')} style={styles.image} />
         </View>
-        <View>
-          <Text style={styles.title}>Contact Austin</Text>
-          <Text>Paragraph talking about reasons to connect with austin or the proper times and what not.</Text>
+        <View style={styles.title}>
+          <Text style={styles.titleText}>Contact Austin</Text>
+          <Text style={styles.contactContent}>Paragraph talking about reasons to connect with austin or the proper times and what not.</Text>
         </View>
-        <View>
-          <Text style={styles.title}>Follow</Text>
-          {/* //this part with have links to
-          //twitter
-          //spotify
-          //facebook
-          //ohter austin shit that i can't even imagine */}
+        <View style={styles.title}>
+          <Text style={styles.titleText}>Follow</Text>
+
+          <View style={styles.contactTouchables}>
+
+            <View style={[styles.button, {backgroundColor: 'rgb(17, 66, 193)'}]}>
+              <TouchableOpacity
+                onPress={this.runFacebook}>
+                <Text style={styles.btnText}>Facebook</Text>
+              </TouchableOpacity>
+            </View>
+
+            <View style={[styles.button, {backgroundColor: 'rgb(31, 193, 17)'}]}>
+              <TouchableOpacity
+                onPress={this.runSpotify}>
+                <Text style={styles.btnText}>Spotify</Text>
+              </TouchableOpacity>
+            </View>
+
+            <View style={[styles.button, {backgroundColor: 'rgb(135, 155, 208)'}]}>
+              <TouchableOpacity
+                onPress={this.runBandcamp}>
+                <Text style={styles.btnText}>Bandcamp</Text>
+              </TouchableOpacity>
+            </View>
+
+          </View>
+
         </View>
       </ScrollView>
     );
   }
-}
+
+  runFacebook = () => {
+    Linking.openURL('https://www.facebook.com/austinmillermusic/?fref=ts');
+  };
+
+  runSpotify = () => {
+    Linking.openURL('https://open.spotify.com/artist/3MEFFWySdL0MYCfJCR1Mej');
+  };
+
+  runBandcamp = () => {
+    Linking.openURL('https://austinmiller.bandcamp.com/');
+  }
+
+
+
+}//ends connect class
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'rgb(193, 78, 247)',
+    backgroundColor: 'rgb(102, 116, 116)',
   },
   imgContainer: {
     justifyContent: 'flex-start',
@@ -42,5 +78,46 @@ const styles = StyleSheet.create({
     borderRadius: 130,
     borderWidth: 3,
     borderColor: 'whitesmoke',
+  },
+  title: {
+    justifyContent: 'center',
+    alignItems: 'stretch',
+    padding: 10,
+    margin: 10,
+    backgroundColor: 'rgb(176, 175, 158)'
+  },
+  contactContent: {
+    backgroundColor: 'whitesmoke',
+    padding: 10,
+    fontSize: 15,
+    fontWeight: '500',
+  },
+  titleText: {
+    color: 'black',
+    fontSize: 20,
+    fontWeight: '500',
+  },
+  button: {
+    padding: 10,
+    margin: 5,
+    borderRadius: 10,
+    borderWidth: 2,
+    borderColor: 'white',
+    shadowColor: 'black',
+    shadowOffset: {
+      width: 0,
+      height: 3
+    },
+    shadowRadius: 5,
+    shadowOpacity: 1.0
+  },
+  btnText: {
+    color: 'white',
+    fontSize: 15,
+  },
+  contactTouchables: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
   }
 });
