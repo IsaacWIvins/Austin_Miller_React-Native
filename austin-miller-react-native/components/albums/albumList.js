df6bf1b687bc53b7aad8d7560f21a2ca9e0a86b3
Isaac Ivins <isaacivins@Isaacs-MacBook-Air.local>
2017-07-08 13:07:23 -0400

    finally got sectionlist appending, needs styling

--- austin-miller-react-native/components/albums/albumList.js
+++ austin-miller-react-native/components/albums/albumList.js
@@ -1,32 +1,21 @@
 import React from 'react';
-import { StyleSheet, Text, View, ListView } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import AlbumListItem from './albumListItem';
 import { Austin } from "../../assets/austinData/austinData";
 
 
 export default class AlbumsList extends React.Component {
 
-  constructor(props) {
-
-    super(props);
-
-    let ds = new ListView.DataSource({
-      rowHasChanged: (r1, r2) => r1 !== r2
-    });
-    this.state = {
-      dataSource: ds.cloneWithRows ( Austin ),
-    }
-  }
-
   render() {
+    // console.log('Austin[0].albums on AlbumList ==========', Austin[0].albums);
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
           Albums
         </Text>
-        <ListView
-          dataSource={this.state.dataSource}
-          renderRow={ ( album ) => <AlbumListItem album={ album } /> } />
+        <View>
+          <AlbumListItem album= { Austin[0].albums } />
+        </View>
       </View>
     );
   }
@@ -37,17 +26,12 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'stretch',
-    backgroundColor: '#111',
+    backgroundColor: 'pink',
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
     color: '#fff',
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#888',
-    marginBottom: 5,
-  },
+  }
 });
--- austin-miller-react-native/components/albums/albumListItem.js
+++ austin-miller-react-native/components/albums/albumListItem.js
@@ -1,29 +1,64 @@
 import React from 'react';
-import { StyleSheet, Text, View, ListView, TouchableHighlight, Image } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, TouchableHighlight, Image, SectionList } from 'react-native';
+
+const extractKey = ({id}) => id;
 
 
 export default class AlbumListItem extends React.Component {
 
-  _handlpress = () => {
-    console.log("this.props.album.albums[0].albumImage", this.props.album.albums[0].albumImage);
-  }
-  render() {
+  _renderSectionHeader = ({ section }) => {
+    console.log("section", section)
     return (
-      <TouchableHighlight onPress={this._handlpress} activeOpacity={ 100 } underlayColor="#ea4b54">
-        <Image source={require(this.props.album.background)} />
-        <Text>{this.props.album.albums[0].title}</Text>
-      </TouchableHighlight>
-    );
+      <View style={styles.dataSection}>
+        <Text>
+          {section.title}
+        </Text>
+      </View>
+    )
+  }
+
+  _renderItems = ({ item }) => {
+    console.log("item", item)
+    return (
+      <View style={styles.dataItem}>
+        <Text>
+          {item.title}
+        </Text>
+      </View>
+    )
+  }
+
+  render() {
+    const { album } = this.props;
+    // console.log("album =============", album);
+    return (
+        <SectionList
+          style={styles.container}
+          sections={album}
+          renderItem={this._renderItems}
+          renderSectionHeader={this._renderSectionHeader}
+          keyExtractor={extractKey}
+        />
+      )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     backgroundColor: 'rgba(0,0,0,0.7)',
     paddingTop: 20,
     paddingBottom: 60,
     paddingLeft: 20,
     paddingRight: 20,
+  },
+  dataItem: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  dataSection: {
+    flex: 1,
+    backgroundColor: 'rgb(135, 111, 230)',
   },
   artistName: {
     color: "#FFF",
