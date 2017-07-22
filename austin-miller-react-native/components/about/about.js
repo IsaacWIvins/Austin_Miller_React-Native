08ce0d3a91884af7480f35f4da19b2a64075d75d
Isaac Ivins <isaacivins@isaacs-air.attlocal.net>
2017-07-22 01:35:54 -0400

    rendering queries on about page

--- austin-miller-react-native/components/about/about.js
+++ austin-miller-react-native/components/about/about.js
@@ -1,52 +1,91 @@
 import React, { Component } from 'react'
-import { AppRegistry, View, StyleSheet, Text } from 'react-native'
+import { ActivityIndicator, AppRegistry, View, ScrollView, StyleSheet, Text } from 'react-native'
 import { graphql, gql } from 'react-apollo'
 
 export class About extends Component {
-  render() {
-    console.log("=============", this.props)
+  _renderArtist = ({ id, images, name, about }) => {
+    console.log("====== id =======", id)
+    console.log("====== name =======", name)
+    console.log("====== images =======", images)
+    console.log("====== about =======", about)
     return (
-        <View style={styles.app}>
-          <Text style={styles.text}>This is the About Page</Text>
+      <View key={id} style={styles.renderContainer}>
+        <View style={styles.nameView}>
+          <Text style={styles.nameText}>{name}</Text>
         </View>
+        <View style={styles.imagesView}>
+          <Text>{name}</Text>
+        </View>
+        <View style={styles.aboutView}>
+          <Text style={styles.aboutText}>{about}</Text>
+        </View>
+      </View>
+    )
+  }
+  render() {
+    // console.log("=============", this.props)
+    const { data } = this.props;
+    const { allArtists, loading } = data
+
+    if (loading) {
+      return <ActivityIndicator />
+    }
+
+    return (
+      <View style={styles.infoContainer}>
+        <ScrollView>
+          {allArtists.map(this._renderArtist)}
+        </ScrollView>
+      </View>
     )
   }
 }
 
 const styles = StyleSheet.create({
-  app: {
+  infoContainer: {
     flex: 1,
     backgroundColor: 'black',
+    alignItems: 'center',
     justifyContent: 'center',
-    alignItems: 'center'
   },
-  text: {
-    color: 'white'
-  }
+  renderContainer: {
+
+  },
+  nameView: {
+
+  },
+  nameText: {
+    color: 'white',
+  },
+  aboutView: {
+
+  },
+  aboutText: {
+    color: 'white',
+  },
 })
 
-export const ARTIST_INFO_QUERY = gql`
-  query albums {
-    allArtists {
-      id
-      name
-      about
-      images {
-        file {
-          name
-          url
-        }
+export const ARTIST_INFO_QUERY =
+gql` query albums {
+  allArtists {
+    id
+    name
+    about
+    images {
+      file {
+        name
+        url
       }
-      albums {
-  			name
-    		image {
-          file {
-  					url
-          }
+    }
+    albums {
+			name
+  		image {
+        file {
+					url
         }
       }
     }
   }
-`
+}`
 
 export default graphql(ARTIST_INFO_QUERY)(About);
