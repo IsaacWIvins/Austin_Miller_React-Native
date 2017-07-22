5e004b469bfcb800a62497669eafc1d9249e5bb3
Isaac Ivins <isaacivins@isaacs-air.attlocal.net>
2017-07-22 05:07:17 -0400

    added styling and query for spotify page, just needs onPress link

--- austin-miller-react-native/components/connect/soundcloud.js
+++ austin-miller-react-native/components/connect/soundcloud.js
@@ -1,19 +1,37 @@
 import React, { Component } from 'react'
 import { AppRegistry, ActivityIndicator, Image, View, StyleSheet, Text } from 'react-native'
-import { graphql, gql } from 'react-apollo';
+import { graphql, gql } from 'react-apollo'
+import { SocialIcon } from 'react-native-elements'
 
 export class Soundcloud extends Component {
+
+  _renderImage = ({file, name}) => {
+    const { url } = file
+    return(
+      <View key={name} style={styles.renderContainer}>
+        <Image
+          source={{uri: url}}
+          resizeMode='cover'
+          style={styles.coverIMG}>
+            <SocialIcon
+              title='SoundCloud'
+              button
+              type='soundcloud'
+            />
+          </Image>
+      </View>
+    )
+  }
+
   render() {
-    // console.log("this.props: ", this.props)
     const { data } = this.props
     const { loading, allImages } = data
     if (loading) {
       return <ActivityIndicator />
     }
-    console.log("allImages", allImages)
     return (
         <View style={styles.container}>
-          <Text style={styles.text}>This is the SOUNDCLOUD Page</Text>
+          {allImages.map(this._renderImage)}
         </View>
     )
   }
@@ -23,12 +41,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'black',
-    justifyContent: 'center',
-    alignItems: 'center'
   },
-  text: {
-    color: 'white'
-  }
+  renderContainer: {
+    flex: 1,
+  },
+  coverIMG: {
+    flex: 1,
+    opacity: .7,
+    height: null,
+    width: null,
+  },
 })
 
 const TumblerQuery = gql`
